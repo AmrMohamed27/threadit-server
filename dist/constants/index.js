ef82c6ebc951953621db44dc0cf20d200502fae8
Amr Mohamed <102391798+AmrMohamed27@users.noreply.github.com>
2025-03-16 17:16:08 +0200

    Delete dist directory

--- dist/constants/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.EXPLORE_COMMUNITIES_COUNT = exports.MAX_REPLY_DEPTH = exports.POSTS_PER_PAGE = void 0;
-exports.POSTS_PER_PAGE = 5;
-exports.MAX_REPLY_DEPTH = 2;
-exports.EXPLORE_COMMUNITIES_COUNT = 4;
-//# sourceMappingURL=index.js.map
